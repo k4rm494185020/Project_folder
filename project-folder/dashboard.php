106e5c7075a92af6bb51d86c0a98c23348e3de36
k4rm494185020 <95732869+k4rm494185020@users.noreply.github.com>
2024-12-05 15:49:52 +0900

    Update dashboard.php

--- project-folder/dashboard.php
+++ project-folder/dashboard.php
@@ -10,12 +10,16 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
     <title>管理画面</title>
 </head>
 <body>
-    <h2>管理画面</h2>
+    <div class="admin-view">
     <p><a href="manage_news.php">新着情報の管理</a></p>
     <p><a href="manage_contacts.php">お問い合わせ内容の管理</a></p>
     <p><a href="logout.php">ログアウト</a></p>
+    </div>
 </body>
 </html>
+
+
